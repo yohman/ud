cafc9ec5aebb0dcae16932161cd91fe66428d3e1
yohda <yohman@dhcp14-222.wireless.ats.ucla.edu>
2016-03-21 11:42:02 -0700

    v1.3 many data updates

--- js/ud.js
+++ js/ud.js
@@ -16,17 +16,55 @@ UD.PopulationDensity2013 = "https://uducla.cartodb.com/api/v2/viz/f744ae74-d69c-
 UD.Majority1990 = "https://uducla.cartodb.com/api/v2/viz/1f07e654-d9a1-11e5-8c3f-0ea31932ec1d/viz.json";
 UD.Majority2000 = "https://uducla.cartodb.com/api/v2/viz/2e72813a-d9a1-11e5-a5a4-0e787de82d45/viz.json";
 UD.Majority2013 = "https://uducla.cartodb.com/api/v2/viz/b47329ec-d9a1-11e5-8155-0e5db1731f59/viz.json";
-UD.pct_lths1990 = "https://uducla.cartodb.com/api/v2/viz/c120adf6-d9ae-11e5-945f-0e5db1731f59/viz.json";
-UD.pct_lths2000 = "https://uducla.cartodb.com/api/v2/viz/18527ae6-d9af-11e5-9d24-0e31c9be1b51/viz.json";
-UD.pct_lths2013 = "https://uducla.cartodb.com/api/v2/viz/2a57fc3e-d9af-11e5-b9a2-0e5db1731f59/viz.json";
 UD.medhhinc1990 = "https://uducla.cartodb.com/api/v2/viz/045c49ee-d9b0-11e5-9489-0e3ff518bd15/viz.json";
 UD.medhhinc2000 = "https://uducla.cartodb.com/api/v2/viz/3cddfa10-d9b0-11e5-a5f7-0e674067d321/viz.json";
 UD.medhhinc2013 = "https://uducla.cartodb.com/api/v2/viz/6099362c-d9b0-11e5-b833-0e31c9be1b51/viz.json";
+UD.pct_lths1990 = "https://uducla.cartodb.com/api/v2/viz/c120adf6-d9ae-11e5-945f-0e5db1731f59/viz.json";
+UD.pct_lths2000 = "https://uducla.cartodb.com/api/v2/viz/18527ae6-d9af-11e5-9d24-0e31c9be1b51/viz.json";
+UD.pct_lths2013 = "https://uducla.cartodb.com/api/v2/viz/2a57fc3e-d9af-11e5-b9a2-0e5db1731f59/viz.json";
+UD.pct_col1990 = "https://uducla.cartodb.com/api/v2/viz/910393cc-eee4-11e5-9b14-0e3ff518bd15/viz.json";
+UD.pct_col2000 = "https://uducla.cartodb.com/api/v2/viz/18bc9b64-eee6-11e5-960b-0e787de82d45/viz.json";
+UD.pct_col2013 = "https://uducla.cartodb.com/api/v2/viz/812a0b50-eee6-11e5-811a-0ecfd53eb7d3/viz.json";
+UD.pct_renter1990 = "https://uducla.cartodb.com/api/v2/viz/a078add0-ef87-11e5-b704-0ecfd53eb7d3/viz.json";
+UD.pct_renter2000 = "https://uducla.cartodb.com/api/v2/viz/f8218850-ef86-11e5-9d68-0ea31932ec1d/viz.json";
+UD.pct_renter2013 = "https://uducla.cartodb.com/api/v2/viz/d0df5908-ef85-11e5-9245-0e674067d321/viz.json";
+UD.mgr1990 = "https://uducla.cartodb.com/api/v2/viz/a6bf57f4-ef8a-11e5-a0a0-0ea31932ec1d/viz.json";
+UD.mgr2000 = "https://uducla.cartodb.com/api/v2/viz/919740e4-ef8a-11e5-97bb-0e5db1731f59/viz.json";
+UD.mgr2013 = "https://uducla.cartodb.com/api/v2/viz/227e52a6-ef8a-11e5-b568-0e3ff518bd15/viz.json";
+UD.pct_rb1990 = "https://uducla.cartodb.com/api/v2/viz/eb7a2598-ef91-11e5-9dbe-0e674067d321/viz.json";
+UD.pct_rb2000 = "https://uducla.cartodb.com/api/v2/viz/ca5807c2-ef91-11e5-9901-0e787de82d45/viz.json";
+UD.pct_rb2013 = "https://uducla.cartodb.com/api/v2/viz/9eaf4726-ef90-11e5-8aab-0ecfd53eb7d3/viz.json";
+
 UD.popden_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/7fcfb40a-d9b3-11e5-b285-0ecfd53eb7d3/viz.json";
 UD.popden_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/05efabbc-d9b4-11e5-badc-0e3ff518bd15/viz.json";
+UD.NHWhite_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/669ac064-eeea-11e5-88cc-0ea31932ec1d/viz.json";
+UD.NHWhite_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/82eb420a-eeec-11e5-a85f-0ecd1babdde5/viz.json";
+UD.LTHS_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/4c34d47c-ef16-11e5-a474-0ecfd53eb7d3/viz.json";
+UD.LTHS_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/0d72c2c0-ef17-11e5-88cc-0ea31932ec1d/viz.json";
+UD.WCD_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/77b0fb4c-ef18-11e5-8909-0e31c9be1b51/viz.json";
+UD.WCD_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/ffd25ed0-ef18-11e5-b594-0ecd1babdde5/viz.json";
+UD.MHI_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/02b51f76-ef81-11e5-83d1-0e5db1731f59/viz.json";
+UD.MHI_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/c35b5876-ef81-11e5-9edf-0e3ff518bd15/viz.json";
+UD.MGR_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/c0d9c510-ef8b-11e5-b4e9-0e31c9be1b51/viz.json";
+UD.MGR_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/87b13060-ef8c-11e5-b568-0e3ff518bd15/viz.json";
+UD.RB_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/79eb1ee0-ef92-11e5-9f6b-0e5db1731f59/viz.json";
+UD.RB_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/2da88422-ef93-11e5-b112-0e5db1731f59/viz.json";
 
 // identify which variables are change parameters so we can hide the year buttons
-UD.changeVars = ['popden_chg_90_00','popden_chg_00_13']
+UD.changeVars = ['popden_chg_90_00','popden_chg_00_13','NHWhite_chg_90_00','NHWhite_chg_00_13','LTHS_chg_90_00','LTHS_chg_00_13','WCD_chg_90_00','WCD_chg_00_13','MHI_chg_90_00','MHI_chg_00_13','MGR_chg_90_00','MGR_chg_00_13','RB_chg_90_00','RB_chg_00_13']
+
+/*
+
+	Map Layers
+
+*/
+var baselayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+});
+var labellayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+});
+
 /*
 
 	On document load
@@ -65,14 +103,14 @@ UD.init = function()
 	};
 	UD.map = new L.Map('cartodb-map', mapOptions);
 
-	var baselayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-	  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-	});
 	UD.map.addLayer(baselayer);
 }
 
 UD.addLayer = function(param,year)
 {
+	// remove labels
+	UD.map.removeLayer(labellayer)
+
 	// get rid of any open tooltip windows and legends
 	$('.cartodb-tooltip').hide();
 	$('.cartodb-legend-stack').hide();
@@ -117,15 +155,19 @@ UD.addLayer = function(param,year)
 	cartodb.createLayer(UD.map, jsonURL,{tooltip:true})
 		.addTo(UD.map)
 		.on('done', function(layer) {
+
 			UD.layer = layer;
 			layer
+			.on('load',function(){
+				UD.map.addLayer(labellayer)
+			})
 			.on('featureOver', function(e, latlng, pos, data) {
-
+				
 			})
 			.on('error', function(err) {
 				console.log('error: ' + err);
 			});
-			var sublayer = layer.getSubLayer(0);
+			// var sublayer = layer.getSubLayer(0);
 
 		}).on('error', function(err) {
 		console.log("some error occurred: " + err);
