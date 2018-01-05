1164085d090bc7937af0d4c191ec10a2d4114166
Yoh Kawano <yohkawano@dhcp14-224.wireless.ats.ucla.edu>
2018-01-05 10:28:57 -0800

    v1.7 Data updates for 2015 and others

--- js/ud.js
+++ js/ud.js
@@ -10,67 +10,51 @@ UD.map2;
 UD.year1 = 1990;
 UD.year2 = 1990;
 UD.param = 'Majority1990';
-UD.PercentPoverty1990 = "https://uducla.cartodb.com/api/v2/viz/8ed490f2-d69c-11e5-ae04-0ecd1babdde5/viz.json";
-UD.PercentPoverty2000 = "https://uducla.cartodb.com/api/v2/viz/a25dc53a-d69c-11e5-8997-0e674067d321/viz.json";
-UD.PercentPoverty2013 = "https://uducla.cartodb.com/api/v2/viz/b7066442-d69c-11e5-b7ec-0ecfd53eb7d3/viz.json";
-UD.PopulationDensity1990 = "https://uducla.cartodb.com/api/v2/viz/d2a7a954-d69c-11e5-a3bd-0ecfd53eb7d3/viz.json";
-UD.PopulationDensity2000 = "https://uducla.cartodb.com/api/v2/viz/e4e44028-d69c-11e5-9e13-0ecfd53eb7d3/viz.json";
-UD.PopulationDensity2013 = "https://uducla.cartodb.com/api/v2/viz/f744ae74-d69c-11e5-acdb-0e31c9be1b51/viz.json";
-UD.Majority1990 = "https://uducla.cartodb.com/api/v2/viz/1f07e654-d9a1-11e5-8c3f-0ea31932ec1d/viz.json";
-UD.Majority2000 = "https://uducla.cartodb.com/api/v2/viz/2e72813a-d9a1-11e5-a5a4-0e787de82d45/viz.json";
-UD.Majority2013 = "https://uducla.cartodb.com/api/v2/viz/b47329ec-d9a1-11e5-8155-0e5db1731f59/viz.json";
-UD.medhhinc1990 = "https://uducla.cartodb.com/api/v2/viz/045c49ee-d9b0-11e5-9489-0e3ff518bd15/viz.json";
-UD.medhhinc2000 = "https://uducla.cartodb.com/api/v2/viz/3cddfa10-d9b0-11e5-a5f7-0e674067d321/viz.json";
-UD.medhhinc2013 = "https://uducla.cartodb.com/api/v2/viz/6099362c-d9b0-11e5-b833-0e31c9be1b51/viz.json";
-UD.pct_lths1990 = "https://uducla.cartodb.com/api/v2/viz/c120adf6-d9ae-11e5-945f-0e5db1731f59/viz.json";
-UD.pct_lths2000 = "https://uducla.cartodb.com/api/v2/viz/18527ae6-d9af-11e5-9d24-0e31c9be1b51/viz.json";
-UD.pct_lths2013 = "https://uducla.cartodb.com/api/v2/viz/2a57fc3e-d9af-11e5-b9a2-0e5db1731f59/viz.json";
-UD.pct_col1990 = "https://uducla.cartodb.com/api/v2/viz/910393cc-eee4-11e5-9b14-0e3ff518bd15/viz.json";
-UD.pct_col2000 = "https://uducla.cartodb.com/api/v2/viz/18bc9b64-eee6-11e5-960b-0e787de82d45/viz.json";
-UD.pct_col2013 = "https://uducla.cartodb.com/api/v2/viz/812a0b50-eee6-11e5-811a-0ecfd53eb7d3/viz.json";
-UD.pct_renter1990 = "https://uducla.cartodb.com/api/v2/viz/a078add0-ef87-11e5-b704-0ecfd53eb7d3/viz.json";
-UD.pct_renter2000 = "https://uducla.cartodb.com/api/v2/viz/f8218850-ef86-11e5-9d68-0ea31932ec1d/viz.json";
-UD.pct_renter2013 = "https://uducla.cartodb.com/api/v2/viz/d0df5908-ef85-11e5-9245-0e674067d321/viz.json";
-UD.mgr1990 = "https://uducla.cartodb.com/api/v2/viz/a6bf57f4-ef8a-11e5-a0a0-0ea31932ec1d/viz.json";
-UD.mgr2000 = "https://uducla.cartodb.com/api/v2/viz/919740e4-ef8a-11e5-97bb-0e5db1731f59/viz.json";
-UD.mgr2013 = "https://uducla.cartodb.com/api/v2/viz/227e52a6-ef8a-11e5-b568-0e3ff518bd15/viz.json";
-UD.pct_rb1990 = "https://uducla.cartodb.com/api/v2/viz/eb7a2598-ef91-11e5-9dbe-0e674067d321/viz.json";
-UD.pct_rb2000 = "https://uducla.cartodb.com/api/v2/viz/ca5807c2-ef91-11e5-9901-0e787de82d45/viz.json";
-UD.pct_rb2013 = "https://uducla.cartodb.com/api/v2/viz/9eaf4726-ef90-11e5-8aab-0ecfd53eb7d3/viz.json";
 
-UD.popden_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/7fcfb40a-d9b3-11e5-b285-0ecfd53eb7d3/viz.json";
-UD.popden_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/05efabbc-d9b4-11e5-badc-0e3ff518bd15/viz.json";
-UD.NHWhite_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/669ac064-eeea-11e5-88cc-0ea31932ec1d/viz.json";
-UD.NHWhite_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/82eb420a-eeec-11e5-a85f-0ecd1babdde5/viz.json";
-UD.LTHS_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/4c34d47c-ef16-11e5-a474-0ecfd53eb7d3/viz.json";
-UD.LTHS_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/0d72c2c0-ef17-11e5-88cc-0ea31932ec1d/viz.json";
-UD.WCD_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/77b0fb4c-ef18-11e5-8909-0e31c9be1b51/viz.json";
-UD.WCD_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/ffd25ed0-ef18-11e5-b594-0ecd1babdde5/viz.json";
-UD.MHI_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/02b51f76-ef81-11e5-83d1-0e5db1731f59/viz.json";
-UD.MHI_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/c35b5876-ef81-11e5-9edf-0e3ff518bd15/viz.json";
-UD.MGR_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/c0d9c510-ef8b-11e5-b4e9-0e31c9be1b51/viz.json";
-UD.MGR_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/87b13060-ef8c-11e5-b568-0e3ff518bd15/viz.json";
-UD.RB_chg_90_00 = "https://uducla.cartodb.com/api/v2/viz/79eb1ee0-ef92-11e5-9f6b-0e5db1731f59/viz.json";
-UD.RB_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/2da88422-ef93-11e5-b112-0e5db1731f59/viz.json";
-UD.jobden2013 = "https://uducla.cartodb.com/api/v2/viz/63f8c478-efc1-11e5-b7c4-0ea31932ec1d/viz.json";
-UD.hcv_rept00 = "https://uducla.cartodb.com/api/v2/viz/7bbdf59c-efda-11e5-bfb9-0e674067d321/viz.json";
-UD.hcv_rept13 = "https://uducla.cartodb.com/api/v2/viz/a956202a-efd9-11e5-9554-0e787de82d45/viz.json";
-UD.lihtc00 = "https://uducla.cartodb.com/api/v2/viz/f1b9bbde-efe2-11e5-8004-0e31c9be1b51/viz.json";
-UD.lihtc13 = "https://uducla.cartodb.com/api/v2/viz/9454d168-efe2-11e5-9edf-0e3ff518bd15/viz.json";
-UD.lihtc_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/e1181dd6-efea-11e5-80f2-0ea31932ec1d/viz.json";
-UD.ttl_housing_05_13 = "https://uducla.cartodb.com/api/v2/viz/84180262-efeb-11e5-8209-0e787de82d45/viz.json";
-UD.condos_05_13 = "https://uducla.cartodb.com/api/v2/viz/fbe10bb8-efeb-11e5-b9af-0e5db1731f59/viz.json";
-UD.sfh_05_13 = "https://uducla.cartodb.com/api/v2/viz/92407bde-efec-11e5-8f03-0e787de82d45/viz.json";
-UD.other_housing_05_13 = "https://uducla.cartodb.com/api/v2/viz/c22ab588-eff3-11e5-869b-0e787de82d45/viz.json";
-UD.pct_condoconv_03_13 = "https://uducla.cartodb.com/api/v2/viz/48526ab6-eff4-11e5-b6da-0ecd1babdde5/viz.json";
-
-UD.hcv_chg_00_13 = "https://uducla.cartodb.com/api/v2/viz/4a8da4c0-efe1-11e5-b704-0ecfd53eb7d3/viz.json";
-UD.upscaling_00_13 = "https://uducla.cartodb.com/api/v2/viz/cf506d7e-eff4-11e5-8004-0e31c9be1b51/viz.json";
-UD.eligible_00_13 = "https://uducla.cartodb.com/api/v2/viz/2e33d7d0-eff6-11e5-b6da-0ecd1babdde5/viz.json";
-UD.gentrify_00_13 = "https://uducla.cartodb.com/api/v2/viz/0b60d61c-eff7-11e5-bcac-0ecfd53eb7d3/viz.json";
+UD.disadvantaged_90_15 = "https://uducla.cartodb.com/api/v2/viz/82e2f3c8-bb0a-4cac-8e1b-9e68831514e1/viz.json";
+UD.gentrify_90_15 = "https://uducla.cartodb.com/api/v2/viz/feb4895a-84eb-4f78-9f23-574ada475daf/viz.json";
+UD.Majority1990 = "https://uducla.cartodb.com/api/v2/viz/31333a59-cc34-471b-858e-f19237d43537/viz.json";
+UD.Majority2000 = "https://uducla.cartodb.com/api/v2/viz/792962d0-01ad-4db6-adc5-9c562df3c08d/viz.json";
+UD.Majority2015 = "https://uducla.cartodb.com/api/v2/viz/14f67500-586d-4d21-b048-3189cf841fd8/viz.json";
+UD.pct_lths1990 = "https://uducla.cartodb.com/api/v2/viz/87881555-501f-4854-8d3a-9df0979d4012/viz.json";
+UD.pct_lths2000 = "https://uducla.cartodb.com/api/v2/viz/325add81-8662-4968-b9f4-a6b165b61eae/viz.json";
+UD.pct_lths2015 = "https://uducla.cartodb.com/api/v2/viz/cc6392df-39d4-421e-8b23-ffb8d81a6ead/viz.json";
+UD.pct_col1990 = "https://uducla.cartodb.com/api/v2/viz/13386940-8ba7-4c66-8f2f-50f92c39f3f9/viz.json";
+UD.pct_col2000 = "https://uducla.cartodb.com/api/v2/viz/e9e372cc-f2d9-4786-bdce-219d3d233c05/viz.json";
+UD.pct_col2015 = "https://uducla.cartodb.com/api/v2/viz/43d0b435-10b1-4284-81dd-955832e66305/viz.json";
+UD.IndividualPovertyRates1990 = "https://uducla.cartodb.com/api/v2/viz/90c66328-9949-44a2-83e8-1654431619bf/viz.json";
+UD.IndividualPovertyRates2000 = "https://uducla.cartodb.com/api/v2/viz/0f474be2-ff0a-4426-a460-c3d72e2a9bdf/viz.json";
+UD.IndividualPovertyRates2015 = "https://uducla.cartodb.com/api/v2/viz/4b7873b4-a786-4905-9051-1de80f6856b7/viz.json";
+UD.MedianHHIncome1990 = "https://uducla.cartodb.com/api/v2/viz/600a3f1d-3f9a-4301-9874-2a98df085ffe/viz.json";
+UD.MedianHHIncome2000 = "https://uducla.cartodb.com/api/v2/viz/bffb3a5c-f678-4b3c-9526-5aabe4bf77cd/viz.json";
+UD.MedianHHIncome2015 = "https://uducla.cartodb.com/api/v2/viz/6c2a0a0a-f879-45f4-8b64-156a98619777/viz.json";
+UD.JobDensity2015 = "https://uducla.cartodb.com/api/v2/viz/97daddd8-8007-4b6f-96a4-b6eb307328ee/viz.json";
+UD.RenterHH1990 = "https://uducla.cartodb.com/api/v2/viz/3d0c8954-244b-4035-a717-309547956e6a/viz.json";
+UD.RenterHH2000 = "https://uducla.cartodb.com/api/v2/viz/4ed5dab8-e129-4c61-a96c-6027e3bf617d/viz.json";
+UD.RenterHH2015 = "https://uducla.cartodb.com/api/v2/viz/aae71883-1494-437e-86d4-b83d1a557ab0/viz.json";
+UD.MedianGrossRent1990 = "https://uducla.cartodb.com/api/v2/viz/85f965ce-1db4-4ed3-90aa-1ed86a19aedc/viz.json";
+UD.MedianGrossRent2000 = "https://uducla.cartodb.com/api/v2/viz/483dcb5d-a7a6-430c-a16f-dfb3dea846e1/viz.json";
+UD.MedianGrossRent2015 = "https://uducla.cartodb.com/api/v2/viz/14f6b67f-c609-42e8-a0ba-dd6ef0735200/viz.json";
+UD.BurdenedRenterHH1990 = "https://uducla.cartodb.com/api/v2/viz/95e447c8-7d2f-47a2-8fd8-ebf6b0f7d8e0/viz.json";
+UD.BurdenedRenterHH2000 = "https://uducla.cartodb.com/api/v2/viz/5fec104c-241e-499e-8289-a56a8dce063e/viz.json";
+UD.BurdenedRenterHH2015 = "https://uducla.cartodb.com/api/v2/viz/b1157d24-b7a7-4df5-bb36-ed108730d5eb/viz.json";
+UD.Section8HH2000 = "https://uducla.cartodb.com/api/v2/viz/34e1a880-ae27-46a0-8d8a-1fc09bea61ef/viz.json";
+UD.Section8HH2015 = "https://uducla.cartodb.com/api/v2/viz/146179d8-09f9-4e5c-ab50-1f646768a774/viz.json";
+UD.ChangeInNonHispanicWhites_1990_2000 = "https://uducla.cartodb.com/api/v2/viz/af846fc7-94ff-4a49-b466-9df0faf01dde/viz.json";
+UD.ChangeInNonHispanicWhites_2000_2015 = "https://uducla.cartodb.com/api/v2/viz/35767520-a9bf-416c-b592-4fa5a9f31e28/viz.json";
+UD.ChangeInCollegeEducated_1990_2000 = "https://uducla.cartodb.com/api/v2/viz/72c8803b-11e1-4386-a672-b7dc0d953170/viz.json";
+UD.ChangeInCollegeEducated_2000_2015 = "https://uducla.cartodb.com/api/v2/viz/f917d515-6121-4854-8cc6-64e0c4175a99/viz.json";
+UD.ChangeInMedianHHIncome_1990_2000 = "https://uducla.cartodb.com/api/v2/viz/491afcf9-ef54-4411-8840-405698ba9d26/viz.json";
+UD.ChangeInMedianHHIncome_2000_2015 = "https://uducla.cartodb.com/api/v2/viz/d6ac019a-54f0-49f0-9cf4-e833eeeae20e/viz.json";
+UD.ChangeInMedianGrossRent_1990_2000 = "https://uducla.cartodb.com/api/v2/viz/7679b5a4-a79f-49ed-8476-4ec8f85a8add/viz.json";
+UD.ChangeInMedianGrossRent_2000_2015 = "https://uducla.cartodb.com/api/v2/viz/89fafa00-b073-4ede-ad93-2d4545a304d9/viz.json";
+UD.ChangeInRentBurdenedHH_1990_2000 = "https://uducla.cartodb.com/api/v2/viz/257cab51-3070-46ce-b137-85914fe8623c/viz.json";
+UD.ChangeInRentBurdenedHH_2000_2015 = "https://uducla.cartodb.com/api/v2/viz/eb610834-8edc-482c-a432-986e7544b3d5/viz.json";
+UD.ChangeInSection8HH_2000_2015 = "https://uducla.cartodb.com/api/v2/viz/4fe8449c-e338-4299-8627-d978b1d9c734/viz.json";
 
 // identify which variables are change parameters so we can hide the year buttons
-UD.changeVars = ['ttl_housing_05_13','popden_chg_90_00','popden_chg_00_13','NHWhite_chg_90_00','NHWhite_chg_00_13','LTHS_chg_90_00','LTHS_chg_00_13','WCD_chg_90_00','WCD_chg_00_13','MHI_chg_90_00','MHI_chg_00_13','MGR_chg_90_00','MGR_chg_00_13','RB_chg_90_00','RB_chg_00_13','jobden2013','hcv_rept00','hcv_rept13','hcv_chg_00_13','lihtc00','lihtc13','lihtc_chg_00_13','condos_05_13','sfh_05_13','other_housing_05_13','pct_condoconv_03_13','upscaling_00_13','eligible_00_13','gentrify_00_13']
+UD.changeVars = ['disadvantaged_90_15','gentrify_90_15','JobDensity2015','Section8HH2000','Section8HH2015','ChangeInNonHispanicWhites_1990_2000','ChangeInNonHispanicWhites_2000','ChangeInCollegeEducated_1990_2000','ChangeInCollegeEducated_2000_2015','ChangeInMedianHHIncome_1990_2000','ChangeInMedianHHIncome_2000_2015','ChangeInMedianGrossRent_1990_2000','ChangeInMedianGrossRent_2000_2015','ChangeInRentBurdenedHH_1990_2000','ChangeInRentBurdenedHH_2000_2015','ChangeInSection8HH_2000_2015']
+
 
 /*
 
@@ -158,23 +142,42 @@ UD.init = function()
 	UD.map1.sync(UD.map2);
 	UD.map2.sync(UD.map1);
 
+	// L.Control.geocoder().addTo(map);
+	
 	// sync maps on pan
-	UD.map1.on('mousedown', function(e) {
-		UD.map1.sync(UD.map2);
-		UD.map2.sync(UD.map1);
-	});
-	UD.map2.on('mousedown', function(e) {
-		UD.map1.sync(UD.map2);
-		UD.map2.sync(UD.map1);
-	});
+	// if(UD.dualmap)
+	// {
+	// 	UD.map1.on('mouseup', function(e) {
+	// 		UD.map1.sync(UD.map2);
+	// 		UD.map2.sync(UD.map1);
+	// 	});
+	// 	UD.map2.on('mouseup', function(e) {
+	// 		UD.map1.sync(UD.map2);
+	// 		UD.map2.sync(UD.map1);
+	// 	});		
+	// }
 
 	// add layer
-	cartodb.createLayer(UD.map1, 'https://uducla.cartodb.com/api/v2/viz/51c3f1ae-067a-11e6-bb72-0ecd1babdde5/viz.json',{tooltip:true})
+	cartodb.createLayer(UD.map1, 'https://uducla.cartodb.com/api/v2/viz/7b22ac78-0596-11e6-b89d-0e31c9be1b51/viz.json',{tooltip:true})
 		// .addTo(UD.map1)
 		.on('done', function(metro) {
 			metro.setZIndex(0);
 			var overlayMaps = {
-				"Metro stops": metro
+				"Metro stops": metro 
+			};
+			L.control.layers(null, overlayMaps).addTo(UD.map1);
+		})
+		.on('error', function(err) {
+			alert("some error occurred: " + err);
+		});
+
+	// add layer
+	cartodb.createLayer(UD.map1, 'https://uducla.cartodb.com/api/v2/viz/3acf9c7d-cf95-4d24-bad3-4698ba344943/viz.json',{tooltip:true})
+		// .addTo(UD.map1)
+		.on('done', function(healthy) {
+			healthy.setZIndex(0);
+			var overlayMaps = {
+				"Building Healthy Communities": healthy
 			};
 			L.control.layers(null, overlayMaps).addTo(UD.map1);
 		})
@@ -190,6 +193,7 @@ UD.init = function()
 UD.addLayer = function(param,year,map2use)
 {
 	console.log(map2use)
+	UD.map1.sync(UD.map2);
 	// hide existing layer
 	if(map2use == UD.map1)
 	{
@@ -292,19 +296,19 @@ UD.addLayer = function(param,year,map2use)
 	});
 }
 
-UD.addMetro = function()
-{
-	// add layer
-	cartodb.createLayer(UD.map1, 'https://uducla.cartodb.com/api/v2/viz/7b22ac78-0596-11e6-b89d-0e31c9be1b51/viz.json',{tooltip:true})
-		.addTo(UD.map1);
+// UD.addMetro = function()
+// {
+// 	// add layer
+// 	cartodb.createLayer(UD.map1, 'https://uducla.cartodb.com/api/v2/viz/7b22ac78-0596-11e6-b89d-0e31c9be1b51/viz.json',{tooltip:true})
+// 		.addTo(UD.map1);
 
-}
+// }
 
 UD.toggleDualMap = function()
 {
 	// resync maps because buggy if you don't
-	UD.map1.sync(UD.map2);
-	UD.map2.sync(UD.map1);
+	// UD.map1.sync(UD.map2);
+	// UD.map2.sync(UD.map1);
 
 	if(UD.dualmap)
 	{
